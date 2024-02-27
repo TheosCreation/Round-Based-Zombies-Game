2330bb231abb94dfe926fd379bae8436833ee611
TheosLaptop <pandazflipperpanda706@gmail.com>
2024-02-27 15:34:39 +1300

    mainmenu is so much better

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    private bool keyPressed;
+    [SerializeField] private GameObject startMenu;
+    [SerializeField] private GameObject lobbyMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private Button playButton;
+    void Update()
+    {
+        if (startMenu.activeSelf == true)
+        {
+            if (Input.anyKey)
+            {
+                playButton.Select();
+                lobbyMenu.SetActive(true);
+                startMenu.SetActive(false);
+            }
+        }
+        if (lobbyMenu.activeSelf == true)
+        {
+        }
+        if (optionsMenu.activeSelf == true)
+        {
+            if (Input.GetKeyDown("escape") || Input.GetKeyDown(KeyCode.JoystickButton1))
+            {
+                playButton.Select();
+                lobbyMenu.SetActive(true);
+                optionsMenu.SetActive(false);
+            }
+        }
+
+    }
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
